26712d23dfbbc88b9d2636536c7046347be661c4
Dice <1121312603@qq.com>
2023-09-20 13:55:35 +0800

    feature:vue project

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>
+    Intro
+    </template>
+    重庆土著，很能吃辣，有rap天赋，
+    <br />
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Education</template>
+    本科：深圳大学计算机科学与技术
+    <br/>
+    硕士：深圳大学计算机技术（在读）
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Hobby</template>
+
+    Taylor Swift，弹琴，唱歌，fifa，瓦，棋，吃，睡
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Comment</template>
+    Here goes nothing.
+  </WelcomeItem>
+</template>
